c03f33400306360f0945dd26ab0bdb1cb67b120a
isaacrdz <zack_cpp@hotmail.com>
2017-09-04 17:37:35 -0500

    store.ninjamkt.com

--- src/pages/products-details/products-details.ts
+++ src/pages/products-details/products-details.ts
@@ -26,11 +26,10 @@ export class ProductsDetailsPage {
     console.log(this.product);
 
     this.WooCommerce = WC({
-      url: "http://localhost:8888/store1/",
-      consumerKey:"ck_371eef5724bae9555956920650fb8b6df14b3444",
-      consumerSecret:"cs_afdd6cba9a4e0e4fcda3353a61005018aabed88d",
-      wpAPI: true,
-      version: 'wc/v1'
+      url: "http://store.ninjamkt.com/",
+      consumerKey:"ck_72513a6f3063cfc8f8e02d4a1a2c27b54e5d9e92",
+      consumerSecret:"cs_3c9ca2bdc0eba9f82bc4a97bc10faffa644eedbc",
+
     });
 
     this.WooCommerce.getAsync('products/'+ this.product.id +'/reviews').then((data)=>{
